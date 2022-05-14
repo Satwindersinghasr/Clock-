057fccf0e76841e1c22a6ce1e4b06c379f4bb1ba
Satwinder Singh <shergill4983@gmail.com>
2022-05-15 00:55:01 +0530

    first commit

---
+++ clk.js
@@ -0,0 +1,16 @@
+let a= new Audio("clk.mp3");
+
+setInterval(()=>{
+    date = new Date();
+    hours = date.getHours();
+    minutes = date.getMinutes();
+    seconds = date.getSeconds();
+    hoursrotation = 30*hours + minutes/2;
+    minutesrotation = 6*minutes;
+    secondsrotation = 6*seconds;
+    
+    hour.style.transform = `rotate(${hoursrotation}deg)`;
+    minute.style.transform = `rotate(${minutesrotation}deg)`;
+    second.style.transform = `rotate(${secondsrotation}deg)`;
+    a.play();
+},1000);
